693421f8113809452977f886b56a4896a75953b4
gregmarquet <marquet.greg@gmail.com>
2018-11-01 22:12:58 -0400

    add conditional in NasaPOD to display iframe in case the link is a video

--- src/components/NasaPOD/NasaPOD.js
+++ src/components/NasaPOD/NasaPOD.js
@@ -12,7 +12,7 @@ class NasaPOD extends Component {
   }
 
   componentDidMount() {
-    fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    fetch('https://api.nasa.gov/planetary/apod?api_key=0KvJyqcBE8xlAMCw12g7oVaIOnVp9SD6bjMeavEV')
       .then(results => results.json())
       .then(data  => this.setState({
         photo: data
@@ -27,10 +27,21 @@ class NasaPOD extends Component {
         <h2 className="pod_title">Nasa Photo Of The Day</h2>
         <h3 className="pod_image-title">                  {this.state.photo.title}
         </h3>
-        <img 
-          className="pod_image"
-          src={this.state.photo.url} 
-          alt={this.state.photo.url}/>
+        {this.state.photo.media_type === 'video' 
+          ? <iframe 
+              title="This is a unique title"
+              type="text/html"
+              width="100%"
+              height="600"
+              src={this.state.photo.url}
+              frameBorder="0">
+            </iframe>
+          : <img
+            className="pod_image"
+            src={this.state.photo.url}
+            alt={this.state.photo.url} />
+        }
+        
         <p className="pod_image-explanation">             {this.state.photo.explanation}
         </p>
       </div>
